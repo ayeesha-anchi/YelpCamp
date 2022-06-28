f1ed73b3028bab48980fe86ac24e4a0860bc7348
ayeesha-anchi <ayeeshaanchi@appinessworld.com>
2022-06-28 09:36:49 +0530

    june28

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
-const flash = require('connect-flash')
-const methodOverride = require('method-override')
-//const Campground = require('./models/campground');
-const campgrounds = require('./routes/campgrounds')
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
-const reviews = require('./routes/reviews')
-// mongoose.connect('mongodb+srv://Ayeesha:Ayeesha840@cluster0.isst9.mongodb.net/camp', {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false
-// });
+const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const helmet = require('helmet');
 
-mongoose.connect('mongodb+srv://Ayeesha:Ayeesha840@cluster0.isst9.mongodb.net/YelpCamp').then((r) => console.log('conneted')).catch(e => console.log(e.message))
+const mongoSanitize = require('express-mongo-sanitize');
+
+
+
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+// const MongoDBStore = require('connect-mongo')(session);
+
+// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = 'mongodb+srv://Ayeesha:Ayeesha840@cluster0.isst9.mongodb.net/test'
+
+mongoose.connect(dbUrl, {
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useUnifiedTopology: true,
+    // useFindAndModify: false
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -26,65 +43,127 @@ db.once("open", () => {
 
 const app = express();
 
-app.engine('ejs',ejsMate)
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname,'views'))
+app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize({
+    replaceWith: '_'
+}))
+
+// const store = new MongoDBStore({
+//     url: dbUrl,
+//     secret: 'thisshouldbeabettersecret!',
+//     touchAfter: 24 * 60 * 60
+// })
+
+// store.on("error", function (e) {
+//     console.log("session store error", e)
+// })
 
 const sessionConfig = {
-    secret: 'thisshouldbeabettersecret!!',
+    // store,
+    name: 'session',
+    secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
+
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/appinessworld/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews',reviews)
+
+
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
+
 
 app.get('/', (req, res) => {
     res.render('home')
-})
+});
+
 
 app.all('*', (req, res, next) => {
-    next(new ExpressError('Page not found', 404))
+    next(new ExpressError('Page Not Found', 404))
 })
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
-    if(!err.message)err.message='Oh no, Something went wrong'
-    res.status(statusCode).render('error',{err});
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
 
-
-
-
-// app.get('/makeCampground', async (req, res) => {
-//     const camp = new Campground({ title: 'myBackyard',description:'cheap camping' });
-//     await camp.save();
-//     res.send(camp);
-// })
-
-app.listen(3000, () => {
-    console.log('listening on port 3000')
+const port = process.env.PORT || 4000;
+app.listen(4000, () => {
+    console.log('Serving on port 4000')
 })
-
-
-
-
-
-
-
--- seeds/index.js
+++ seeds/index.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const {places,descriptors}=require('./seedHelpers')
+const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-// mongoose.connect('mongodb+srv://Ayeesha:Ayeesha840@cluster0.isst9.mongodb.net/camp', {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true
-// });
-
-mongoose.connect('mongodb+srv://Ayeesha:Ayeesha840@cluster0.isst9.mongodb.net/YelpCamp').then((r) => console.log('conneted')).catch(e => console.log(e.message))
+mongoose.connect('mongodb+srv://Ayeesha:Ayeesha840@cluster0.isst9.mongodb.net/test', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
@@ -19,17 +18,36 @@ db.once("open", () => {
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
+
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++){
+    for (let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            location: `${cities[random1000].city},${cities[random1000].state}`,
+            //YOUR USER ID
+            author: '62a1ddacf9d54519fab5e6ac',
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image:'https://source.unsplash.com/collection/483251',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
-            price
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/appinessworld/image/upload/v1654775643/YelpCamp/inkcz6chmo2szsx8swlx.jpg',
+                    filename: 'YelpCamp/inkcz6chmo2szsx8swlx'
+                },
+                {
+                    url: 'https://res.cloudinary.com/appinessworld/image/upload/v1654775643/YelpCamp/qx76ooslh7ieycdatbss.jpg',
+                    filename: 'YelpCamp/qx76ooslh7ieycdatbss'
+                }
+            ]
         })
         await camp.save();
     }
